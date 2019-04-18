36a05f44aba915adaa040f60748c5f77b0e3704c
Juan Carlos Santiago <krlos013@gmail.com>
2019-04-17 22:43:26 -0500

    tree commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,42 +1,41 @@
 class HomeController < ApplicationController
   def index
     
-    require 'json'
+    
     require 'uri'
     require 'net/http'
     require 'rest_client'
+    require 'json'
     if params[:token].blank? 
       @url = Setting.all.pluck(:urlReditectionTokenEmpty).first
       redirect_to @url
     else
       @endPoint = Setting.all.pluck(:urlEndPoint).first
       @parametro = Setting.all.pluck(:token).first
+      @ApiKey = Setting.all.pluck(:apiKey).first
       @urlRedirectionTokenError = Setting.all.pluck(:urlRedirectionTokenError).first
      begin
-     
-        uri = URI(@endPoint)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
-        request = Net::HTTP::Get.new(uri, {'Content-Type' =>'application/json'})
-        request.body = {@parametro => params[:token]}.to_json
-        res = http.request(request)
-        result = JSON.parse(res.body)
-        puts "response #{res.body}"
+
+     res =  RestClient.post "#{@endPoint}", {"ApiKey" => @ApiKey.to_s, "#{@parametro}" => params[:token], "Domain" => "registro.nnilatam.com"}.to_json, {content_type: :json, accept: :json}
+        hash = JSON.parse res
+        re = hash['Data']
+        firstName = JSON(re)['Email']
+        lastName = JSON(re)['Email']
+        nickName = JSON(re)['Email']
+        email = JSON(re)['Email']
         if res.nil?
 
-         #puts "response #{res.body}"
-         #puts JSON.parse(res.body)
-         redirect_to @urlRedirectionTokenError
+        # redirect_to @urlRedirectionTokenError
          
          
         else
-          @firstName = result['Data']['FirstName']
-         @lastName = result['Data']['LastName']
-         @nickName = result['Data']['NickName']
-         @email = result['Data']['Email']
-         @events = Event.order(:id).page params[:page]
-         @events_registred = EventRegister.where(:email =>  @email).pluck(:id)
+         
+          @firstName = firstName
+          @lastName = lastName
+          @nickName = nickName
+          @email    = email
+          @events = Event.order(:id).page params[:page]
+          @events_registred = EventRegister.where(:email =>  @email).pluck(:id)
         end
   
         if (@events_registred.blank?) 
